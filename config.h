60d90655f883aa50cb9478f923912dfc9d16ae80
PabloBernalPolo <pablo.bernal.polo@gmail.com>
2017-03-29 10:21:36 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2017 P.Bernal-Polo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 
+ * File:   config.h
+ * Author: P.Bernal-Polo
+ * 
+ * Created on February 19, 2017, 2:05 PM
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define MUKF_O
+#define MUKF_RP
+#define MUKF_MRP
+#define MUKF_RV
+#define MEKF_O
+#define MEKF_RP
+#define MEKF_MRP
+#define MEKF_RV
+#define Madgwick_AHRS
+
+#define W0 1.0/25.0
+
+#define CHART_UPDATE
+
+//#define R1e_2
+//#define R1e_4
+//#define R1e_6
+
+
+#endif /* CONFIG_H */
+
